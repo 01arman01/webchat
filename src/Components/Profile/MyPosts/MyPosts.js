07d24a69619ad71e686c118c0fa527bc22b8a2e5
Arman Papyan <58137792+01arman01@users.noreply.github.com>
2022-03-08 00:43:51 +0400

    main posts style

--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -1,13 +1,21 @@
-import react from "react"
-import s from './MyPosts.module.css'
-const MyPosts = ()=>{
-     return(
-                 <div className={s.mayposts}>
-                     <input type="text" className={s.newPost }/>
-                     <button type='button' className={s.add_button}>Add</button>
-                 </div>
-     )
-}
+import react from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
+const MyPosts = () => {
+  return (
+    <div className={s.mayposts}>
+      <div>
+        <input type="text" className={s.newPost} />
+        <button type="button" className={s.add_button}>
+          Add
+        </button>
+      </div>
 
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
 
-export default  MyPosts
+export default MyPosts;
--- src/Components/Profile/MyPosts/Post/Post.js
+++ src/Components/Profile/MyPosts/Post/Post.js
@@ -1,11 +1,16 @@
-import react from 'react'
-
-const Post = ()=>{
-    return(
-        <div>
-            <div>post 1</div>
-        </div>
-    )
-}
-
-export default Post
+import react from "react";
+import s from "./Post.module.css";
+const Post = () => {
+  return (
+    <div className={s.post}>
+      <div>
+        <img
+          src="https://cdn1.vectorstock.com/i/1000x1000/31/95/user-sign-icon-person-symbol-human-avatar-vector-12693195.jpg"
+          className={s.post_ava}
+        />
+      </div>
+      <div>post 1</div>
+    </div>
+  );
+};
+export default Post;
